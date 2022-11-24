98f12da82e36b3da2af7ebac0275fff92d94ea8a
242349 Andrzejczak Sławomir <242349@edu.p.lodz.pl>
2022-11-24 09:27:13 +0100

    Template of game

---
+++ script.js
@@ -0,0 +1,39 @@
+window.addEventListener('load',function (){
+    //canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d'); //argument for webGL or DX, means that we work in 2d not 3d
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class InputHandler{
+
+    }
+
+    class Particle {
+
+    }
+
+    class Projectile{
+
+    }
+
+    class Player{
+
+    }
+
+    class Enemy{
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+    class UI{           //draw score and so on
+
+    }
+})
